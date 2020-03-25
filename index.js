8a06970426e6c2a623e7c3bda7d2bd22ae22389f
pft\\Ruby_Feng <Ruby_Feng@PerfectCorp.com>
2020-03-25 18:32:17 +0800

    'update0325'

--- index.js
+++ index.js
@@ -10,23 +10,23 @@ app.get('/', (req, res) => {
     res.sendFile( __dirname + '/view/index.html');
 });
 
+//express放靜態資源
 app.use('/css', express.static('css'));
 app.use('/asset', express.static('asset'));
 
+//socket連接
 io.on('connection', (socket) => {
+    //連接上+1
     onlineCount++;
     io.emit("online", onlineCount);
 
-    socket.on("greet", () => {
-        console.log('greet');
-        socket.emit("greet", "Hi! Client.");
-    });
-
+    //離開連接-1
     socket.on('disconnect', () => {
         onlineCount = (onlineCount < 0) ? 0 : onlineCount-=1;
         io.emit("online", onlineCount);
     });
 
+    //接受到有人送出訊息
     socket.on("send", (msg) => {
         if (Object.keys(msg).length < 2) return;
         io.emit("msg", msg);
